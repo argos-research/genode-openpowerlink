8a12a5319d47b4c982dc7c4bbf09f531e3fd1566
Axel Fischer <axel.fischer1@gmx.de>
2018-01-23 13:53:40 +0100

    Added wrapper template

--- kernel/edrv/genode_wrapper.cpp
+++ kernel/edrv/genode_wrapper.cpp
@@ -1,22 +1,24 @@
-#include "genode_wrapper.h"
-
-#include <nic_session/nic_session.h>
-
-#include <base/log.h>
-
 extern "C" {
-  void get_Mac_Address(UINT8 addr[6]) {
-    Genode::log("Getting MAC Address");
-    //Get Mac Address from Genode NIC
-    struct Nic::Session nic;
-    Net::Mac_address mac_address;
 
-    mac_address = nic.mac_address();
+	#include "genode_wrapper.h"
 
-    mac_address.copy(addr);
-  }
+	#include <nic_session/nic_session.h>
 
-  void init_Session() {
+	#include <base/log.h>
 
-  }
+
+  	void get_Mac_Address(UINT8 addr[6]) {
+	    Genode::log("Getting MAC Address");
+    	//Get Mac Address from Genode NIC
+    	struct Nic::Session nic;
+    	Net::Mac_address mac_address;
+
+    	mac_address = nic.mac_address();
+
+	    mac_address.copy(addr);
+  	}
+
+	  void init_Session() {
+
+  	}
 }
