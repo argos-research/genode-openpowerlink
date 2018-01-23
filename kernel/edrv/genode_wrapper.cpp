2e7d734407a6b6d8071567022333098b674378c8
Axel Fischer <axel.fischer1@gmx.de>
2018-01-23 13:49:14 +0100

    Added wrapper template

--- kernel/edrv/genode_wrapper.cpp
+++ kernel/edrv/genode_wrapper.cpp
@@ -1,5 +1,8 @@
-//C++-source: sys_unicode_for_c.hpp
 #include "genode_wrapper.h"
+
+#include <nic_session/nic_session.h>
+
+#include <base/log.h>
 
 extern "C" {
   void get_Mac_Address(uint8_t addr[6]) {
@@ -14,6 +17,6 @@ extern "C" {
   }
 
   void init_Session() {
-  	
+
   }
 }
--- kernel/edrv/genode_wrapper.h
+++ kernel/edrv/genode_wrapper.h
@@ -1,12 +1,6 @@
-//C/C++ - header "sys_unicode_for_c.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-  #include <nic_session/nic_session.h>
-
-  #include <base/log.h>
 
   void  get_Mac_Address(uint8_t addr[6]);
 
