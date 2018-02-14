31ca18d38f15bc6b4a9016de8214247627e0afa3
Axel <axel.fischer1@gmx.de>
2018-02-14 21:10:15 +0100

    Added test file

--- app/test.c
+++ app/test.c
@@ -1,5 +1,18 @@
-//#include <base/env.h>
-//#include <base/printf.h>
+#include <oplk/oplk.h>
+#include <oplk/debugstr.h>
+
+#include <system/system.h>
+#include <obdcreate/obdcreate.h>
+#include <getopt/getopt.h>
+#include <console/console.h>
+#include <eventlog/eventlog.h>
+
+#if defined(CONFIG_USE_PCAP)
+#include <pcap/pcap-console.h>
+#endif
+
+#include <stdio.h>
+#include <limits.h>
 
 int main()
 {
