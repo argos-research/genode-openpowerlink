55d70e0f52254476b04a189c9800a4b5c4e6cb5e
Axel <axel.fischer1@gmx.de>
2018-02-15 00:28:08 +0100

    Added test file

--- contrib/trace/trace-printf.c
+++ contrib/trace/trace-printf.c
@@ -58,13 +58,13 @@ The function prints a debug trace message using standard printf
 //------------------------------------------------------------------------------
 void trace(const char* fmt, ...)
 {
-    va_list argptr;
-    char * output;
+//    va_list argptr;
+//    char * output;
 
-    va_start(argptr, fmt);
-    vsprintf(output, fmt, argptr);
+//    va_start(argptr, fmt);
+//    vsprintf(output, fmt, argptr);
 //    printConsole((char const *)output);
     printConsole("DEBUG MSG!!!!!!!!!!");
 //    vfprintf(stderr, fmt, argptr);
-    va_end(argptr);
+//    va_end(argptr);
 }
