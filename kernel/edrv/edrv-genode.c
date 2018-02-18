900c148b5d5c3d3b482038e9c3b41ef3a8293a91
Axel Fischer <axel.fischer1@gmx.de>
2018-02-18 23:47:27 +0100

    Changed edrv structure to threads

--- kernel/edrv/edrv-genode.c
+++ kernel/edrv/edrv-genode.c
@@ -1,8 +1,6 @@
 //------------------------------------------------------------------------------
 // includes
 //------------------------------------------------------------------------------
-#include <kernel/edrv.h>
-
 #include "genode_wrapper.h"
 
 #include <common/oplkinc.h>
@@ -45,13 +43,7 @@
 
 This structure describes an instance of the Ethernet driver.
 */
-typedef struct
-{
-    tEdrvInitParam      initParam;                          ///< Init parameters
-    tEdrvTxBuffer*      pTransmittedTxBufferLastEntry;      ///< Pointer to the last entry of the transmitted TX buffer
-    tEdrvTxBuffer*      pTransmittedTxBufferFirstEntry;     ///< Pointer to the first entry of the transmitted Tx buffer
-    void*               genodeEthThread;
-} tEdrvInstance;
+
 //------------------------------------------------------------------------------
 // local vars
 //------------------------------------------------------------------------------
