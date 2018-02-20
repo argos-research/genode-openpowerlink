b4c6ba68d6f8431de9516dbe2846fd06799b9339
Axel <axel.fischer1@gmx.de>
2018-02-20 18:46:15 +0100

    Changed driver

--- kernel/edrv/genode_wrapper.cpp
+++ kernel/edrv/genode_wrapper.cpp
@@ -113,6 +113,7 @@ class Nic_receiver_thread : public Genode::Thread_deprecated<8192>
 			
 		        //rxBuffer.pBuffer = *rx_content; 
 			//Genode::memcpy(rxBuffer.pBuffer, rx_content, rx_packet.size());
+			memcpy(rxBuffer.pBuffer, rx_content, rx_packet.size());
 
                     	// Call Rx handler of Data link layer
                     	retReleaseRxBuffer = init->initParam.pfnRxHandler(&rxBuffer);
